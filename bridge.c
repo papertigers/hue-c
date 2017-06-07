6ef9050090cf431ef98a7f18483a4654e419f684
Michael Zeller <mike@mikezeller.net>
2017-06-07 19:55:31 -0400

    use socklen_t instead of int

--- bridge.c
+++ bridge.c
@@ -14,7 +14,7 @@ char *msearch = "M-SEARCH * HTTP/1.1\r\n"
 		"HOST:239.255.255.250:1900\r\n"
 		"MAN:\"ssdp:discover\"\r\n"
 		"ST:ssdp:all\r\n"
-		"MX:1"; 
+		"MX:1";
 
 
 static void
@@ -28,19 +28,19 @@ main()
 {
 	int sockfd;
 	struct sockaddr_in sockaddr_bind, sockaddr_send, sockaddr_recv;
-	int recvlen = sizeof(sockaddr_recv);
+	socklen_t recvlen = sizeof(sockaddr_recv);
 	char buf[BUFFLEN];
-	
-	if((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) 
+
+	if((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
 		fatal("Failed to create socket");
-	
+
 	// Setup sockaddr's
-	bzero(&sockaddr_bind, sizeof(sockaddr_bind));	
+	bzero(&sockaddr_bind, sizeof(sockaddr_bind));
 	sockaddr_bind.sin_family = AF_INET;
 	sockaddr_bind.sin_port = htons(0);
 	sockaddr_bind.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	bzero(&sockaddr_send, sizeof(sockaddr_send));	
+	bzero(&sockaddr_send, sizeof(sockaddr_send));
 	sockaddr_send.sin_family = AF_INET;
 	sockaddr_send.sin_port = htons(1900);
 	sockaddr_send.sin_addr.s_addr = inet_addr("239.255.255.250");
@@ -48,8 +48,8 @@ main()
 	if (bind(sockfd, (struct sockaddr *)&sockaddr_bind,
 				sizeof(sockaddr_bind)) == -1)
 		fatal("Failed to bind socket");
-	
-	if (sendto(sockfd, msearch, strlen(msearch), 0, 
+
+	if (sendto(sockfd, msearch, strlen(msearch), 0,
 		(struct sockaddr *)&sockaddr_send, sizeof(sockaddr_send)) == -1)
 		fatal("Failed to send discovery packet");
 
@@ -58,8 +58,8 @@ main()
 		// TODO check for errno and break loop
 		recvfrom(sockfd, buf, BUFFLEN, 0,
 			(struct sockaddr *)&sockaddr_recv, &recvlen);
-		if (strstr(buf, "IpBridge") != NULL) 
-			printf("Received packet from %s:%d\n", 
+		if (strstr(buf, "IpBridge") != NULL)
+			printf("Received packet from %s:%d\n",
 				inet_ntoa(sockaddr_recv.sin_addr),
 					ntohs(sockaddr_recv.sin_port));
 	}
